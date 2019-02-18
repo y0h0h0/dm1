75a89be775f73e53c6b0329222032a915334c777
Tom Teaser <y0h0h0@yandex.ru>
2019-02-18 23:49:30 +0300

    env

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 const express = require('express')
 // const path = require('path')
 const PORT = process.env.PORT || 5000
+require('dotenv').config()
+
 
 express()
   // .use(express.static(path.join(__dirname, 'public')))
   // .set('views', path.join(__dirname, 'views'))
   // .set('view engine', 'ejs')
   // .get('/', (req, res) => res.render('pages/index'))
-  .get('/', (req, res) => res.send('daa'))
+  .get('/', (req, res) => res.send('daa ' + process.env.MNAM))
   // .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))

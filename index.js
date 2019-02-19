51996d12caf0f18967f806e097480d4d6bc4cc6b
Tom Teaser <y0h0h0@yandex.ru>
2019-02-19 13:28:38 +0300

    some routes

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-// const cool = require('cool-ascii-faces')
 const express = require('express')
-// const path = require('path')
+const path = require('path')
 const PORT = process.env.PORT || 5000
 require('dotenv').config()
 
+const app = express();
 
-express()
-  // .use(express.static(path.join(__dirname, 'public')))
-  // .set('views', path.join(__dirname, 'views'))
-  // .set('view engine', 'ejs')
-  // .get('/', (req, res) => res.render('pages/index'))
-  .get('/', (req, res) => res.send('daa ' + process.env.MNAM))
-  // .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  app.use(express.static('public'));
+
+  app.use('/lol', (req, res) => res.send('lol path '));
+
+  app.use('/clan1', (req, res) => {
+    var request = require('request');
+    request('https://alpha.tl/api?clan=1', function (error, response, body) {
+      let parsedBody = JSON.parse(body);
+      res.send( parsedBody.name  + ' ___ ' + parsedBody.country )
+    });
+  }
+  );
+
+
+
+  app.use(function(req, res, next) {
+    res.status(404).send('Sorry cant find that!');
+  });
+
+
+
+
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))

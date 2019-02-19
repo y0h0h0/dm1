a700c2b9dadda5aec928add1163c5a374a2434c2
Tom Teaser <y0h0h0@yandex.ru>
2019-02-19 13:43:13 +0300

    cors

--- apis/testapi.js
+++ apis/testapi.js
@@ -1,5 +1,11 @@
 // в файле subroute
+const express = require('express')
 const router = require('express').Router(); // фишка в роутере
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
 
 router.get('/getme', (req,res)=>{
   // res.send('im a route of getme');
